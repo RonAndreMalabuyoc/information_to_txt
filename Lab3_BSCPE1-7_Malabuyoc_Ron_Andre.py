9d5ed1d322e1369ece0d8979aac0de1e8d0dbb98
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2025-04-06 16:44:09 +0800

    input and repeat

---
+++ Lab3_BSCPE1-7_Malabuyoc_Ron_Andre.py
@@ -0,0 +1,14 @@
+#Write a method in python to write multiple line of text contents into a text file mylife.txt
+
+#While true loop to make sure for another input
+while True:
+    try:
+#Input to put in text file
+        my_file_input = input("Please put in you're personal details, Please: ")
+
+#check for another input
+        Another = input("Will you like to put more of your personal information in? y/n ")
+        if Another == "n":
+            break
+    except:
+        print("Let's do this again shall we.")
