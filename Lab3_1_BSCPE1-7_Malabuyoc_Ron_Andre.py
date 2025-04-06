d32204ba8019d3c5dd975788fdcb9248107ae71c
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2025-04-06 18:09:11 +0800

    finished

--- Lab3_1_BSCPE1-7_Malabuyoc_Ron_Andre.py
+++ Lab3_1_BSCPE1-7_Malabuyoc_Ron_Andre.py
@@ -1,14 +1,19 @@
 #Write a method in python to write multiple line of text contents into a text file mylife.txt
 
-#While true loop to make sure for another input
+##While true loop to make sure for another input
 while True:
     try:
-#Input to put in text file
+##Input to put in text file
         my_file_input = input("Please put in you're personal details, Please: ")
 
-#check for another input
+        with open ("mylife.txt", "a") as file:
+            file.write(my_file_input + "\n")
+            print("Appended to mylife.txt")
+##check for another input
         Another = input("Will you like to put more of your personal information in? y/n ")
         if Another == "n":
             break
     except:
         print("Let's do this again shall we.")
+
+print("Appended to mylife.txt")
